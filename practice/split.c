a1f057e95e73f73ad85ae13fed2757b7eed6cc77
kenkeluke <halimahogunbiyi@gmail.com>
2022-08-10 23:40:13 +0100

    Fixing split

--- practice/split.c
+++ practice/split.c
@@ -9,14 +9,14 @@
 */
 char **split(char *line)
 {
-	char **array;
-	char *delim = "\n ";
-	char *token = strtok(line, delim);
+	char *delim = " ";
 	void *p = NULL;
-	unsigned int n_word, i;
+	unsigned int n_word = 1, i;
+	char **array = malloc( n_word * sizeof(char *));
+	char *token = strtok(line, delim);
 
-
-	array = malloc(	1 * sizeof(char*));
+	
+	
 	if (array == NULL)
 	{
 		free(array);
@@ -25,21 +25,25 @@ char **split(char *line)
 
 	/*fill the array with tokens from the line*/
 	array[0] = token;
-	n_word = 1;
 	printf("word -> %s\n", array[0]);
 
 	for (i = 1; token != NULL; i++)
 	{
-		array = _realloc(p, i, i+1);
-		printf("%s\n", array[i]);
+		
 		token = strtok(NULL, delim);
-		array[i] = token;
-
-		printf("word -> %s\n", array[i]);
-		n_word++;
+		if (token != NULL)
+		{
+			n_word++;
+			array = realloc(array, n_word * sizeof(char *));
+			array[i] = token;
+			printf("word -> %s\n", array[i]);
+			
+		}
+		
+		
 	}
 	printf("array reallocated with size %u\n", i - 1);
-	printf("num of words = %u\n", n_word - 1);
+	printf("num of words = %u\n", n_word - 1 );
 	return (array);
 }
 
