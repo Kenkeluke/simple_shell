0da89ac91d60c10fd650ee77bce5c0e0af0860fe
omniah7 <om267165@gmail.com>
2022-08-11 05:17:35 +0200

    fixing

--- functions.c
+++ functions.c
@@ -74,49 +74,6 @@ int _strcmp(char* s1, char* s2)
 	return (dif);
 }
 
-/*
-* 
-* 
-*/
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * *_strcmp - compares two strings
- * @s1: the first char pointer
- * @s2: the second char pointer
- * Return: 0 if they're equal, any other integer otherwise
- */
-int _strcmp(char* s1, char* s2)
-{
-	int i, dif;
-
-	dif = 0;
-	for (i = 0; (s1[i] != '\0') || (s2[i] != '\0'); i++)
-	{
-		dif = s1[i] - s2[i];
-		if (s1[i] != s2[i])
-			return (dif);
-	}
-	return (dif);
-}
 /**
  * *_strcpy - copies a string into another
  * @src: the source string pointer
