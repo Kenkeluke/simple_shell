561fee1b64f64463e1d5c47314f46d45d2a73aa3
kenkeluke <halimahogunbiyi@gmail.com>
2022-08-11 02:35:25 +0100

    re-arranging files

---
+++ split.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+* split - splits a string into words
+* @line: the string to be splitted
+* @n_word: the address of n_word variable
+* @linesize: the sizeof the line given by getline
+* 
+* Return: an array of each word of the string
+*/
+char **split(char *line)
+{
+	char *delim = "\n ";
+	void* p = NULL;
+	unsigned int n_word = 1, i;
+	char **array = malloc( n_word * sizeof(char *));
+	char *token = strtok(line, delim);
+
+	
+	
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	}
+
+	/*fill the array with tokens from the line*/
+	array[0] = token;
+	printf("word -> %s\n", array[0]);
+
+	for (i = 1; token != NULL; i++)
+	{
+		
+		token = strtok(NULL, delim);
+		if (token != NULL)
+		{
+			n_word++;
+			array = _realloc(p, n_word - 1, n_word);
+			array[i] = token;
+			printf("word -> %s\n", array[i]);
+			
+		}
+
+	}
+	free(line);
+	printf("array reallocated with size %u\n", n_word);
+	array[n_word] = NULL;
+
+	return (array);
+}
+
