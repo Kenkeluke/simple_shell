43d731500d7f50e3ab5579367ca9e66cc08f1471
kenkeluke <halimahogunbiyi@gmail.com>
2022-08-13 04:24:50 +0100

    formatting code

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 
 /**
 * main - prints "$ ", wait for the user to enter a command, execute it
+*@ac: number of arguments
+*@av: argument list
+*@env: environment
 * Return: 0 on success. -1 otherwise
 */
 int main(int ac __attribute__((unused)), char **av, char **env)
@@ -12,7 +15,7 @@ int main(int ac __attribute__((unused)), char **av, char **env)
 	while (1)
 	{
 		char *line = NULL, **words = NULL;
-		
+
 
 		if (isatty(STDIN_FILENO) == 1)
 		{
@@ -26,7 +29,7 @@ int main(int ac __attribute__((unused)), char **av, char **env)
 		rline(&line, active);
 		words = split(line, av[0]);
 
-		if (words == NULL || line == NULL || words [1] == NULL)
+		if (words == NULL || line == NULL || words[1] == NULL)
 		{
 			free(line);
 			free(words);
